278b898718de33db7ee347e5e830bea4a824cc66
Wei Wang <weizhi.station.wang@gmail.com>
2015-03-05 20:01:12 -0500

    clean up

--- ecosim1.0/runEcoSim.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-:: Find the absolute path to this script, and change to that directory.
-set ABS_PATH=%~d0%~p0
-cd /D "%ABS_PATH%"
-:: Execute EcoSim.
-cmd /c java -jar bin\EcoSim.jar %*
-
